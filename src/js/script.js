352b2807dddb97955141495afd96ce10ad50321a
Dmitry Pavlenko <104529636+jokino9212@users.noreply.github.com>
2023-01-15 18:04:01 +0300

    fix burger

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', function () {
 	document.querySelector('#burger').addEventListener('click', function () {
+		document.querySelector('#burger').classList.toggle('header__burger-button--is-active')
 		document.querySelector('#nav').classList.toggle('header__nav-is-active')
 	})
 })
