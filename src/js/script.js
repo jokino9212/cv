f0224b72b71460215fa9a247c5ed097e4fcf572a
Dmitry Pavlenko <104529636+jokino9212@users.noreply.github.com>
2023-01-16 11:10:19 +0300

    fix burger

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,19 @@
 const nav = document.getElementById('nav');
 const burger = document.getElementById('burger');
+
+
+window.addEventListener('DOMContentLoaded', function () {
+	burger.addEventListener('click', function () {
+		burger.classList.toggle('header__burger-button--is-active')
+		nav.classList.toggle('header__nav--is-active')
+	})
+})
+
 document.addEventListener('click', (e) => {
 	const clickBurger = e.composedPath().includes(burger);
 	const clickNav = e.composedPath().includes(nav);
-		if (!clickBurger && !clickNav) {
-			burger.classList.remove('header__burger-button--is-active')
-			nav.classList.remove('header__nav--is-active')
-		}
-})
-
-window.addEventListener('DOMContentLoaded', function () {
-	document.getElementById('burger').addEventListener('click', function () {
-		document.getElementById('burger').classList.toggle('header__burger-button--is-active')
-		document.getElementById('nav').classList.toggle('header__nav--is-active')
-	})
+	if (!clickBurger && !clickNav) {
+		burger.classList.remove('header__burger-button--is-active')
+		nav.classList.remove('header__nav--is-active')
+	}
 })
